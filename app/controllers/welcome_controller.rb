76b9dd49793386e24b9fc5554e64d001eaeaff5d
kuocode <support@saying.tw>
2020-12-20 18:18:08 +0800

    add something.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+    def index
+        # flash[:notice] = "早安！您好！"
+        flash[:warning] = "晚安！该睡了！"
+        # flash[:alert] = "早安！您好！"
+
+    end
 end
