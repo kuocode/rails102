296dd52b16dfc4396a324a7703383c3acf3f4ea0
kuocode <support@saying.tw>
2020-12-20 19:04:48 +0800

    add something.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,11 @@ class GroupsController < ApplicationController
         @group = Group.find(params[:id])
     end
 
+    def edit
+        @group = Group.find(params[:id])
+    end
+
+
     def new
         @group = Group.new
     end
@@ -14,7 +19,20 @@ class GroupsController < ApplicationController
     def create
         @group = Group.new(group_params)
         @group.save
-            redirect_to groups_path
+        redirect_to groups_path
+    end
+
+    def update
+        @group = Group.find(params[:id])
+        @group.update(group_params)
+        redirect_to groups_path, notice: "更新成功！"
+    end
+
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        flash[:alert] = "群组已删除！"
+        redirect_to groups_path
     end
 
     private
