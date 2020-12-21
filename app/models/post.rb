ac2cb1bca1cfdcfaccca0a4d39a6c5d53b53db64
kuocode <support@saying.tw>
2020-12-22 00:01:48 +0800

    add something.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
     belongs_to :group
 
     validates :content, presence: true
-    scope :recent, -> {order("cerated_at DESC")}
+    scope :recent, -> { order("created_at DESC")}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+    
     has_many :groups
+    has_many :participated_groups, :through => :group_relationships, :source => :group
 end
