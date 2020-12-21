97ef13e8e66195766c8186f2e0c191fb81940689
kuocode <support@saying.tw>
2020-12-22 00:26:23 +0800

    add something.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-    
+
     has_many :groups
+    
+    has_many :group_relationships
     has_many :participated_groups, :through => :group_relationships, :source => :group
+
+    def is_member_of?(group)
+        participated_groups.include?(group)
+    end
 end
