2198376471ba4300ba170a40f3c089109d929a4c
kuocode <support@saying.tw>
2020-12-23 00:04:28 +0800

    add something.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-    before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+    before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy, :join, :quit]
     before_action :find_group_and_check_permission, only:[:edit, :update, :destroy]
 
     def index
@@ -42,6 +42,28 @@ class GroupsController < ApplicationController
         @group.destroy
         flash[:alert] = "群组已删除！"
         redirect_to groups_path
+    end
+
+    def join
+        @group = Group.find(params[:id])
+        if !current_user.is_member_of?(@group)
+            current_user.join!(@group)
+            flash[:notice] = "加入本讨论版成功！"
+        else
+            flash[:warning] = "你已经是本组成员！"
+        end
+        redirect_to group_path(@group)
+    end
+
+    def quit
+        @group = Group.find(params[:id])
+        if current_user.is_member_of?(@group)
+            current_user.quit!(@group)
+            flash[:alert] = "已退出本讨论版！"
+        else
+            flash[:alert] = "你不是本讨论版成员，无法退出！"
+        end
+        redirect_to group_path(@group)
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     devise_for :users
 
     resources :groups do
+        member do
+            post :join
+            post :quit
+        end
         resources :posts
         # resources :jobs 自动创建遵循 RESTful 的 url 格式。
     end
