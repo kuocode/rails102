73ed15723385817d48012ff0277574d09e893801
kuocode <support@saying.tw>
2020-12-23 20:06:13 +0800

    add something.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!, :only => [:new, :create]
+    before_action :find_group_and_post, only:[:edit, :update, :destroy]
 
     def new
         @group = Group.find(params[:group_id])
         @post = Post.new
+    end
+
+    def edit
     end
 
     def create
@@ -19,10 +23,30 @@ class PostsController < ApplicationController
         end
     end
 
+    def update
+        if @post.update(post_params)
+            redirect_to account_posts_path, notice: "更新成功！"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @post.destroy
+        # flash[:alert] = "群组已删除！"
+        redirect_to account_posts_path, alert: "群组已删除！"
+    end
+
+
     private
 
     def post_params
         params.require(:post).permit(:content)
+    end
+
+    def find_group_and_post
+        @group = Group.find(params[:group_id])
+        @post = @group.posts.find(params[:id])
     end
 
 end
